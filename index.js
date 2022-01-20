496878cd56b44c1ef820bd31a0f9a96c6449a3f2
parth5795 <20504917@students.ltu.edu.au>
2022-01-20 17:13:06 +1100

    New changes

--- index.js
+++ index.js
@@ -1,2 +1,7 @@
-// Make a video chatting room
-// Start coding 
+//fatal: No remote for the current branch.
+//fatal: Your current branch 'master' does not have a remote configured.
+//fatal: You must specify a remote branch to merge into.
+//fatal: You must specify a commit to merge.
+//fatal: Your configuration specifies to merge with the ref 'refs/heads/master', but that ref does not exist.
+//fatal: You must specify a commit to merge.
+//fatal: Your configuration specifies to merge with the ref 'refs/heads/master', but that ref does not exist.       
